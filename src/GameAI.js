39339b4e78bdb08d6373d6b001cbbba23d1e39e8
RJ <oshmagoshzz@gmail.com>
2018-04-18 03:11:02 -0400

    file structure with beautify

--- src/GameAI.js
+++ src/GameAI.js
@@ -1,5 +1,3 @@
-
-
 const getAvailableSpots = (board) => {
   let result = [];
   for (let i = 0; i < board.length; i++) {
@@ -12,20 +10,36 @@ const getAvailableSpots = (board) => {
 
 const calculateWinner = (board) => {
   const winningRows = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+    [
+      0, 1, 2
+    ],
+    [
+      3, 4, 5
+    ],
+    [
+      6, 7, 8
+    ],
+    [
+      0, 3, 6
+    ],
+    [
+      1, 4, 7
+    ],
+    [
+      2, 5, 8
+    ],
+    [
+      0, 4, 8
+    ],
+    [
+      2, 4, 6
+    ]
   ];
   for (let i = 0; i < winningRows.length; i++) {
     const [a, b, c] = winningRows[i];
-    if (board[a] && board[a] === board[b] && board[a] === board[c]){
-       return true && board[a];
-     }
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return true && board[a];
+    }
   }
   return false;
 };
@@ -85,7 +99,7 @@ const maximize = (board) => {
     }
     board[moves[i]] = null;
   }
-  
+
   return [bestValue, bestMove];
 };
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-
 import React from 'react';
-import { render } from 'react-dom';
+import {render} from 'react-dom';
 import Board from './Board';
 import './index.css'
-render(<Board />, document.getElementById('main'));
+render(<Board/>, document.getElementById('main'));
