802dddd10938a1c6c8090c8f439b4b4ab554f9ba
Ryan Paige <oshmagoshzz@gmail.com>
2021-05-15 22:43:10 -0400

    updated todos

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Board from "./Board.js";
 import { AppWrapper } from "../view/styles.js";
 import Github from "../view/github";
-//todos: multiple boards, scorekeeper, different square styling on click
+//todos: multiple boards/games, scorekeeper counter, different styling on click
 const App = () => {
   return (
     <AppWrapper>
