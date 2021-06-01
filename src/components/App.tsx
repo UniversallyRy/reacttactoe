f1bbe504cb29ec450fb4d02643da948f6c9d7f2a
Ryan Paige <oshmagoshzz@gmail.com>
2021-06-01 14:49:32 -0400

    to tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Board from "./Board.js";
+import Board from "./Board.jsx";
 import { AppWrapper } from "../view/styles.js";
 import Github from "../view/github";
 //todos: multiple boards/games, scorekeeper counter, different styling on click
