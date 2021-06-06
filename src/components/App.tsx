f2ed52576fc860700c9015367db771e7fa76b115
Ryan Paige <oshmagoshzz@gmail.com>
2021-06-05 22:16:15 -0400

    comment cleanup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Board from "./Board";
 import { AppWrapper } from "../view/styles";
 import Github from "../view/github";
-//todos: multiple boards/games, scorekeeper counter, different styling on click
+//todos: multiple boards/games, scorekeeper counter
 const App = () => {
   return (
     <AppWrapper>
