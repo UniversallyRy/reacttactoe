32860f78bd9895f4394cb259df414f3c375c3cd1
Ryan Paige <oshmagoshzz@gmail.com>
2021-06-26 02:39:17 -0400

    square tests

--- src/components/Squares.test.tsx
+++ src/components/Squares.test.tsx
@@ -1,10 +1,19 @@
 import ReactDOM from "react-dom";
-// import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Squares from "./Squares";
+
+const state = {squares: [], myTurn: false} 
+const handleClick = (() => null);
 
 test("renders without crashing", () => {
   const div = document.createElement("div");
-  const state = {squares: [], myTurn: false}  
-  const handleClick = (() => null);
   ReactDOM.render(<Squares i={0} state={state} handleClick={handleClick}/>, div);
+});
+
+test('check default square color', () => {
+  const { container } = render(<Squares i={0} state={state} handleClick={handleClick}/>);
+  const square = container.querySelector('.sc-iCoGMd.jYXntD')
+  expect(square).toHaveStyleRule('background', '#cbb4d4')
+  expect(square).toMatchSnapshot()
+  expect(square).toBeInTheDocument();
 });
